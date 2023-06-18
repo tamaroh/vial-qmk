662916abd0144198738729e72cb26a579fdcf638
Tamaroh <tamaroh@gmail.com>
2023-06-18 18:20:41 +0900

    added vial keymaps and vial.json

---
+++ keyboards/maggie42/keymaps/vial/config.h
@@ -0,0 +1,7 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x1A, 0xF9, 0x91, 0xD7, 0x48, 0xA3, 0x39, 0x0A}
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 2 }
+#define VIAL_UNLOCK_COMBO_COLS { 5, 5 }
